ca1b3b4c839c391e613dc35a956f9bf2a66364c7
Milton Sabogal <111940162+MiltonSabogal@users.noreply.github.com>
2024-02-24 20:16:43 -0500

    segundo commit

--- index.js
+++ index.js
@@ -1,32 +1,47 @@
+const { error } = require('console');
 const express = require('express')//importar la libreria 
 const app = express()//iniciacion de la variable para iniciar la libreria
 const router = express.Router();//enrutar los servicios web
 const port = 3000; //escuchar la ejecucion del servidor
 const mongoose = require('mongoose');//importo la libreria moongose
-mongoose.connect('mongodb+srv://miltonsabogalmintic:80248785.Milton@cluster0.9t8tm0m.mongodb.net/')
-app.use(express.urlencoded({extended:true}))
+mongoose.connect('mongodb+srv://miltonsabogalmintic:80248785.Milton@cluster0.9t8tm0m.mongodb.net/talentotech')
+const UserSchema = require('./models/User')
+app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
-
 
 
 router.get('/', (req, res) => {
     res.send("Hello world")
 })
 
-
-router.get('/user', (req, res) => {
+router.get('/user', async (req, res) => {
     //console.log(req.params.name)
-    res.send("Me ejecute por get")
+    let users = await UserSchema.find();
+    res.json(users)
+})
+
+router.get('/user/:id', async (req, res) => {
+    //console.log(req.params.name)
+    var id = req.params.id
+    let user = await UserSchema.findById(id);
+    console.log(user)
+    res.json(user)
+    //traer un  usuario pasandole el email
+    // const query = UserSchema.where({email: email});
+    // const user = await query.findOne()
 })
 
 router.post('/user', (req, res) => {
-//    var user = {
-//        "Username": req.body.name,
-//        "lastname": req.body.lastname,
-//        "age": req.body.age,
-//    }
-//    res.send(user)
-res.send("Me ejecute por post")
+    let user = UserSchema({
+        name: req.body.name,
+        lastname: req.body.lastname,
+        email: req.body.email,
+        id: req.body.id,
+        password: req.body.password
+    })
+
+    user.save()
+
 })
 
 router.patch('/user', (req, res) => {
@@ -34,9 +49,9 @@ router.patch('/user', (req, res) => {
     //     "Username": req.body.name,
     //     "lastname": req.body.lastname,
     //     "age": req.body.age,
-        
-        
-   // }
+
+
+    // }
     res.send("Me ejecute por patch")
 
 })
@@ -56,7 +71,7 @@ router.delete('/user', (req, res) => {
 
 
 app.use(router)
-app.listen(port, () =>{
-    console.log('listen on ' + port )
+app.listen(port, () => {
+    console.log('listen on ' + port)
 
 })
