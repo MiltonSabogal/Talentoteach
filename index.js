923e0dc6ec2fc36a056f377723b3dbc430b33459
Milton Sabogal <111940162+MiltonSabogal@users.noreply.github.com>
2024-02-29 14:51:18 -0500

    segundo commit

--- index.js
+++ index.js
@@ -5,72 +5,19 @@ const router = express.Router();//enrutar los servicios web
 const port = 3000; //escuchar la ejecucion del servidor
 const mongoose = require('mongoose');//importo la libreria moongose
 mongoose.connect('mongodb+srv://miltonsabogalmintic:80248785.Milton@cluster0.9t8tm0m.mongodb.net/talentotech')
-const UserSchema = require('./models/User')
+const UserRoutes = require('./routes/UserRoutes')
+
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
+
 
 
 router.get('/', (req, res) => {
     res.send("Hello world")
 })
 
-router.get('/user', async (req, res) => {
-    //console.log(req.params.name)
-    let users = await UserSchema.find();
-    res.json(users)
-})
-
-router.get('/user/:id', async (req, res) => {
-    //console.log(req.params.name)
-    var id = req.params.id
-    let user = await UserSchema.findById(id);
-    console.log(user)
-    res.json(user)
-    //traer un  usuario pasandole el email
-    // const query = UserSchema.where({email: email});
-    // const user = await query.findOne()
-})
-
-router.post('/user', (req, res) => {
-    let user = UserSchema({
-        name: req.body.name,
-        lastname: req.body.lastname,
-        email: req.body.email,
-        id: req.body.id,
-        password: req.body.password
-    })
-
-    user.save()
-
-})
-
-router.patch('/user', (req, res) => {
-    // var user = {
-    //     "Username": req.body.name,
-    //     "lastname": req.body.lastname,
-    //     "age": req.body.age,
-
-
-    // }
-    res.send("Me ejecute por patch")
-
-})
-
-router.delete('/user', (req, res) => {
-    // var user = {
-    //     "Username": req.body.name,
-    //     "lastname": req.body.lastname,
-    //     "age": req.body.age,
-    // }
-    res.send("Me ejecute por Delete")
-
-})
-
-
-
-
-
 app.use(router)
+app.use('/', UserRoutes)
 app.listen(port, () => {
     console.log('listen on ' + port)
 
---
+++ routes/UserRoutes.js
@@ -0,0 +1,95 @@
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcrypt');
+const UserSchema = require('../models/User')
+
+router.get('/user', async (req, res) => {
+    //console.log(req.params.name)
+    let users = await UserSchema.find();
+    res.json(users)
+})
+
+router.get('/user/:id', async (req, res) => {
+    //console.log(req.params.name)
+    var id = req.params.id
+    let user = await UserSchema.findById(id);
+    console.log(user)
+    res.json(user)
+    //traer un  usuario pasandole el email
+    // const query = UserSchema.where({email: email});
+    // const user = await query.findOne()
+})
+
+router.post('/user', async (req, res) => {
+    try {
+
+
+        const hashedPassword = await bcrypt.hash(req.body.password, 10)
+        let user = UserSchema({
+            name: req.body.name,
+            lastname: req.body.lastname,
+            email: req.body.email,
+            id: req.body.id,
+            password: hashedPassword
+        })
+
+        user.save()
+        res.send(user)
+    } catch (error) {
+        console.log(error)
+        res.send('error almacenando la informacion')
+    }
+
+})
+
+router.patch('/user/:id', (req, res) => {
+    var id = req.params.id
+
+    var updateUser = {
+
+        name: req.body.name,
+        lastname: req.body.lastname,
+        email: req.body.email,
+        id: req.body.id
+
+    }
+    UserSchema.findByIdAndUpdate(id, updateUser, { new: true }).then((result) => {
+        res.send(result)
+
+    }).catch((error) => {
+        console.log(error)
+        res.send('error actualizando el registro')
+
+    })
+})
+
+router.delete('/user/:id', (req, res) => {
+    var id = req.params.id
+    UserSchema.deleteOne({ _id: id }).then(() => {
+
+        res.json({ "status": "success", "message": "User deleted successfully" })
+
+    }).catch((error) => {
+        console.log(error)
+        res.json({ "status": "failed", "message": "Erro deleting user" })
+    })
+
+})
+//Ejemplo 2
+// var name = req.params.name
+// var email = req.params.email
+// var query;
+// if(email != null){
+//     query = {name: name, email: email}
+// }else{
+//     query = {name: name}
+// }
+// //Puedo establecer cualquier parametro para eliminar
+//     UserSchema.deleteOne(query).then(() => {
+//         res.json({"status": "success", "message": "User deleted successfully"})
+//     }).catch((error) => {
+//         console.log(error)
+//         res.json({"status": "failed", "message": "Error deleting user"})
+//     })
+
+module.exports = router
